50f1791fcfbb8d6e7cb1c34201e12d0327dc7be3
neralwar <ashish.neralwar@gmail.com>
2020-06-04 20:49:15 +0530

    Added sample programming file

--- Python Programming Basics/lambda.py
+++ Python Programming Basics/lambda.py
@@ -9,25 +9,17 @@ print((lambda x,y,z,c : x+y*z*c)(1,2,3,4))
 
 #Filter Function along with Lambda function inline
 
-from functools import reduce 
-
-ages = [5, 12]
-
-reduce_result = reduce(lambda num1, num2: num1 + num2, ages, 0)
-
-print(reduce_result)
-
-
-#adults = list(filter(lambda x: x < 18 , ages))
-#print(list(filter(lambda x: x < 18 , ages)))
-#print(adults)
+ages = [5, 12, 17, 18, 24, 32]
+adults = list(filter(lambda x: x < 18 , ages))
+print(list(filter(lambda x: x < 18 , ages)))
+print(adults)
 
 #map() functional applies to  each item in Iterable list Tuple and set
-#my_list = [1, 5, 4, 6, 8, 11, 3, 12]
+my_list = [1, 5, 4, 6, 8, 11, 3, 12]
 
-#new_list = list(map(lambda x: x * 2 , my_list))
+new_list = list(map(lambda x: x * 2 , my_list))
 
-#print(new_list)
+print(new_list)
 
 
 
