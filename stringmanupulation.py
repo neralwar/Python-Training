e8618bbf8d3bec5c35dc73e293ebdf93be85691c
neralwar <ashish.neralwar@gmail.com>
2020-06-04 19:32:28 +0530

    Remove Deleted files

--- stringmanupulation.py
+++
@@ -1,7 +0,0 @@
-a="edureka python"
-print(a.split('u'))
-
-import urllib.request
-
-
-urllib.request.urlretrieve("https://accounts.google.com/SignOutOptions?hl=en&continue=https://www.google.com%3Fhl%3Den-US", "localfilename.png")
--- var.py
+++
@@ -1,9 +0,0 @@
-def func(*args):
-   for i in args:
-       print(i)
-func(3,2,1,4,5)
-
-
-a=b=c=10
-
-print(a,b,c)
